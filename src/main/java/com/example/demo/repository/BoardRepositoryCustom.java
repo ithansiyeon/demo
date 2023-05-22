4ab2f9cbf28fbfc019d009700c5e7a9c89c4a538
taiji5@kimyoung.co.kr <itsiyeon@nate.com>
2023-05-22 16:27:52 +0900

    Revert "Initial commit"
    This reverts commit 0309aa0919a20907df8061786ebddd05cc95dc43.

--- src/main/java/com/example/demo/repository/BoardRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Board;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface BoardRepositoryCustom {
-    Page<Board> findBoardCustom(Pageable pageable);
-}
--- src/main/java/com/example/demo/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-    Member findById(String name);
-
-}
