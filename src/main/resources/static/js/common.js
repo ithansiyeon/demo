87b0910bc5288ccf74b5a1eab07e776b7fc32f73
siyeon <itsiyeon@nate.com>
2023-07-04 22:47:30 +0900

    메뉴 사이드 바 수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,59 +1,22 @@
 $(document).ready(function(){
-	/* lnb */
-	(function($){
+	authorityMenu();
 
-		var lnbUI = {
-			click : function (target, speed) {
-				var _self = this,
-					$target = $(target);
-				_self.speed = speed || 300;
+	// gnb
+	$("nav > ul > li.has_sub > a").click(function(e){
+		alert("aaaaa");
+		if($(this).parent().has("> ul")) {
+			e.preventDefault();
+		}
 
-				$target.each(function(){
-					if(findChildren($(this))) {
-						return;
-					}
-					$(this).addClass('noDepth');
-				});
-
-				function findChildren(obj) {
-					return obj.find('> ul').length > 0;
-				}
-
-				$target.on('click','a', function(e){
-					e.stopPropagation();
-					var $this = $(this),
-						$depthTarget = $this.next(),
-						$siblings = $this.parent().siblings();
-
-					$this.parent('li').find('ul li').removeClass('on');
-					$siblings.removeClass('on');
-					$siblings.find('ul').slideUp(250);
-
-					if($depthTarget.css('display') == 'none') {
-						_self.activeOn($this);
-						$depthTarget.slideDown(_self.speed);
-					} else {
-						$depthTarget.slideUp(_self.speed);
-						_self.activeOff($this);
-					}
-
-				})
-
-			},
-			activeOff : function($target) {
-				$target.parent().removeClass('on');
-			},
-			activeOn : function($target) {
-				$target.parent().addClass('on');
-			}
-		};
-
-		// Call lnbUI
-		$(function(){
-			lnbUI.click('#lnb li', 300)
-		});
-
-	}(jQuery));
+		if(!$(this).hasClass("on")) {
+			$(this).next("ul").stop().slideDown(200);
+			$(this).addClass("on");
+			$(this).parent().siblings().find(" > a").removeClass("on").end().find(" > ul").stop().slideUp(200);
+		}else if($(this).hasClass("on")) {
+			$(this).removeClass("on");
+			$(this).next("ul").stop().slideUp(200);
+		}
+	});
 
 	// cm_list
 	$(".cm_list > div > a").click(function(){
